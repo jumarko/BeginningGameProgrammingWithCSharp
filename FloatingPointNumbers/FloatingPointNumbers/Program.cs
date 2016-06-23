386650c999c61371bdd8780197fd4093cdb7069b
Juraj Martinka <juraj@snapshot.travel>
2016-06-23 12:59:07 +0200

    Add example for Week 1 - Lecture 3.4 Real numbers

---
+++ FloatingPointNumbers/FloatingPointNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FloatingPointNumbers
+{
+	/// <summary>
+	/// Demonstrates floating point numbers
+	/// </summary>
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int score = 1356;
+			int totalSecondsPlayed = 10000;
+
+			// Watch Out! The result is zero!!! -> Integer division
+			float pointsPerSecond = score / totalSecondsPlayed;
+			Console.WriteLine("Points per second: " + pointsPerSecond);
+			Console.WriteLine();
+
+			// force floating-point divison
+			float pointsPerSecondFloat = (float) score / totalSecondsPlayed;
+			Console.WriteLine("Points per second: " + pointsPerSecondFloat);
+			Console.WriteLine();
+		}
+	}
+}
