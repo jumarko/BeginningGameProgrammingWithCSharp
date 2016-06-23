aaf0703cb346457244dc2de53234d55cbf945d97
Juraj Martinka <juraj@snapshot.travel>
2016-06-23 13:03:04 +0200

    Add Lab1 for Week 1

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lab1
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, my name is Juraj Martinka.");
+
+			// I can try to store my name in a variable
+			string myName = "Juraj Martinka";
+			Console.WriteLine("Hello again, my name is: " + myName);
+
+			Console.WriteLine();
+
+			Console.WriteLine("Name of my best friend is Lucia Haronikova");
+
+			Console.WriteLine();
+
+			Console.WriteLine("Names of my three favorite games:");
+			Console.WriteLine("Settlers 2");
+			Console.WriteLine("Warcraft 3");
+			Console.WriteLine("Gobblins");
+
+			Console.WriteLine();
+		}
+	}
+}
