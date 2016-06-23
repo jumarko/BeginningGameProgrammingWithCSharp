a1100e29b5342ef9ee35d17ceff4b5937b18d501
Juraj Martinka <juraj@snapshot.travel>
2016-06-23 12:46:26 +0200

    Add example for Week 1 - Lecture 3.3 Integer Data Types

---
+++ IntegerDataTypes/IntegerDataTypes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace IntegerDataTypes
+{
+	/// <summary>
+	/// Demonstrates integer data types
+	/// </summary>
+	class MainClass
+	{
+		/// <summary>
+		/// Demonstrates integer data types
+		/// </summary>
+		/// <param name="args">The command-line arguments.</param>
+		public static void Main(string[] args)
+		{
+			// you can let compiler to figure out variable type but don't do it regularly
+			// var seconds = 100;
+
+			// declare integer variable and constant
+			int totalSecondsPlayed = 100;
+			const int SECONDS_PER_MINUTE = 60;
+
+			// calculate minutes and seconds
+			int minutes = totalSecondsPlayed / SECONDS_PER_MINUTE;
+			int seconds = totalSecondsPlayed % SECONDS_PER_MINUTE;
+
+			// print results - integer division
+			Console.WriteLine("Minutes: " + minutes);
+			Console.WriteLine("Seconds: " + seconds);
+
+			// overflow - but compiler will detect it!
+			// Console.WriteLine("Result: " + (2147483647 + 1));
+
+			Console.WriteLine();
+		}
+	}
+}
