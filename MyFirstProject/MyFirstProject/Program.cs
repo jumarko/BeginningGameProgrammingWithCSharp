c08fa96dc9e106bc1a205cce1626e125ba679de3
Juraj Martinka <juraj@snapshot.travel>
2016-06-23 12:22:03 +0200

    MyFirstProject added to repo - very basic console application from Week 1 - Lecture 2.2

---
+++ MyFirstProject/MyFirstProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyFirstProject
+{
+	class MainClass
+	{
+		/// <summary>
+		///  Main method to print message
+		/// </summary>
+		/// <param name="args">The command-line arguments.</param>
+		public static void Main(string[] args)
+		{
+			// print a message
+			Console.WriteLine("Hi, smart n00b");
+
+			Console.WriteLine();
+		}
+	}
+}
