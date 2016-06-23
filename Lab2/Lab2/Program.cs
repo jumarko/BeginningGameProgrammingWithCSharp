bf8c6581c4787a96840263fb9d3fcd92db99e8d4
Juraj Martinka <juraj@snapshot.travel>
2016-06-23 13:07:41 +0200

    Add Lab2 for Week 1

---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lab2
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			printAge();
+
+			Console.WriteLine();
+
+			printScore();
+		}
+
+		private static void printAge()
+		{
+			int age = 30;
+			Console.WriteLine("Hello, my age is: " + age);
+
+		}
+
+		private static void printScore()
+		{
+			const int MAX_SCORE = 100;
+			int score = 58;
+			float percent = (float) score / MAX_SCORE * 100;
+			Console.WriteLine("Score percentage is: " + percent + "%");
+			Console.WriteLine();
+
+		}
+	}
+}
