902ec1b44769222a7fe346882363dcdf1279315f
Juraj Martinka <juraj@snapshot.travel>
2016-07-06 22:24:40 +0200

    Add examples for String processing lectures.

---
+++ IntroToStrings/IntroToStrings/IntroToStrings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace IntroToStrings
+{
+	/// <summary>
+	///  Contains examples from String lecture:
+	/// - https://www.coursera.org/learn/game-programming/lecture/knjII/6-1-intro-to-strings
+	/// </summary>
+	class StringIO
+	{
+		public static void Main(string[] args)
+		{
+			// first lecture
+
+			// prompt for and read in gamertag
+			Console.Write("Enter gamertag: ");
+			string gamertag = Console.ReadLine();
+
+			// prompt for and read in level
+			Console.Write("Enter level: ");
+
+			int level = int.Parse(Console.ReadLine());
+
+			char firstGamertagCharacter = gamertag[0];
+
+			// print out values
+			Console.WriteLine();
+			Console.WriteLine("Gamertag: " + gamertag);
+			Console.WriteLine("Level: " + level);
+			Console.WriteLine("First Gamertag character: " + firstGamertagCharacter);
+		}
+	}
+
+}
---
+++ StringsProcessing/StringsProcessing/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace StringsProcessing
+
+{
+	/// <summary>
+	///  Contains examples from String lecture:
+	/// - https://www.coursera.org/learn/game-programming/lecture/1jQEC/6-2-string-operations
+	/// </summary>
+	class StringsProcessing
+	{
+		public static void Main(string[] args)
+		{
+			// read in csv string
+			Console.Write("Enter name and percent (name,percent): ");
+			string csvString = Console.ReadLine();
+
+			// find comma location
+			int commaLocation = csvString.IndexOf(",", StringComparison.Ordinal);
+
+			// extract name and percent
+			string name = csvString.Substring(0, commaLocation);
+			float percent = float.Parse(csvString.Substring(commaLocation + 1));
+
+			// print name and percent;
+			Console.WriteLine();
+			Console.WriteLine("name: " + name);
+			Console.WriteLine("percent: " + percent);
+		}
+	}
+}
