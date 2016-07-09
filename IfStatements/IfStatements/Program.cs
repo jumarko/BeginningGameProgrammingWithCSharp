767e35e2a70fa1e8248ee8774b15af581f3fb594
Juraj Martinka <juraj@snapshot.travel>
2016-07-09 07:12:35 +0200

    Week 3 - Examples with If and Switch

---
+++ IfStatements/IfStatements/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace IfStatements
+{
+	/// <summary>
+	/// Demonstrates various forms of if statement
+	/// </summary>
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			// ask for and get user answer
+			Console.Write("Pick up the shiny thing (y, n): ");
+			char answer = Console.ReadLine()[0];
+
+			// print appropriate message
+			if (answer == 'y' || answer == 'Y')
+			{
+				Console.WriteLine("You have the shiny object.");
+			}
+			else if (answer == 'n' || answer == 'N')
+			{
+				Console.WriteLine("You don't have the shiny object.");
+			}
+			else
+			{
+				Console.WriteLine("You are a n00b");
+			}
+
+			Console.WriteLine();
+		}
+	}
+}
---
+++ SwitchStatements/SwitchStatements/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SwitchStatements
+{
+	/// <summary>
+	/// Demonstrates switch statements
+	/// </summary>
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			// ask for and get user answer
+			Console.WriteLine("Pick up the shiny thing? (y, n): ");
+			char answer = Console.ReadLine()[0];
+
+			// print appropriate message
+			switch (answer)
+			{
+				case 'y':
+				case 'Y':
+					Console.WriteLine("You have the shiny thing");
+					break;
+				case 'n':
+				case 'N':
+					Console.WriteLine("You don't have the shiny thing");
+					break;
+				default:
+					Console.WriteLine("You are n00b.");
+					break;
+			}
+		}
+	}
+}
